e743a3a1c5abcfd1f867daa18813045e23db2beb
SidMinecraft <68089579+SidMinecraft@users.noreply.github.com>
2020-11-13 12:52:03 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const RPC = require("discord-rpc");
+const rpc = new RPC.Client({
+    transport: "ipc"
+})
+
+rpc.on("ready", () => {
+    rpc.setActivity({
+        details: "Editing Sequence 2",
+        state: "Untitled.vpj",
+        largeImageKey: "image",
+        largeImageText: "Using Videopad Pro v 8.63",
+    })
+
+    console.log("RPC active");
+})
+
+rpc.login({
+    clientId: "776550128071999519"
+})
